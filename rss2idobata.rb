2b9d364e3e03b2ee68b16b56df97a1026f2443be
Yohei YASUKAWA <yasulab@gmail.com>
2015-10-02 21:37:05 +0900

    Initial commit inspired from hatebu2idobata

--- rss2idobata.rb
+++ rss2idobata.rb
@@ -5,17 +5,13 @@ require 'rss'
 require 'idobata'
 
 Idobata.hook_url = ENV['IDOBATA_END']
-HATEBU_USERS = [
-  "YassLab",
-  # "Other Username"
-]
 
 msg = ""
 HATEBU_USERS.each { |user|
   # Flush cache RSS before downloading
   `curl -H 'Pragma: no-cache' -L b.hatena.ne.jp/#{user}/rss`
 
-  rss = RSS::Parser.parse("http://b.hatena.ne.jp/#{user}/rss")
+  rss = RSS::Parser.parse("http://www.freee.co.jp/blog/feed")
 
   # NOTE: Heroku Scheduler's frequency should be set to "Every 10 minutes"
   bookmarks = rss.items.select do |item|
@@ -23,7 +19,7 @@ HATEBU_USERS.each { |user|
   end
 
   msg << bookmarks.map {|b|
-    p "<a href='#{b.link}'>#{b.title}</a> by <span class='label label-info'>#{user}</span><br /> <b>#{b.description}<b/>"
+    p "<a href='#{b.link}'>#{b.title}</a> by <span class='label label-info'>freee</span><br /> <b>#{b.description}<b/>"
   }.join("<br/>")
 }
 
